dbee5f0eb3c2f2f86495215c7e4b380dbc039923
Y00521 <114495211+Y00521@users.noreply.github.com>
2022-09-27 21:38:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,33 +63,33 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '汪汪队副队长',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ozFlF6kBbGUhfP-EV2WNnujocyyY',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 所在省份
-      province: '广东',
+      province: '东北',
       // 所在城市
-      city: '肇庆',
+      city: '沈阳',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-26',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'https://wangxinleo.cn',
+      openUrl: 'https://www.bilibili.com/n',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '汪汪队副队长', year: '2004', date: '11-26',
         },
         {
           type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '汪汪队副队长', year: '1996', date: '09-31',
         },
         {
           type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
@@ -239,9 +239,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '广东',
+  PROVINCE: '东北',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '惠州',
+  CITY: '沈阳',
 
   /** 重要节日相关 */
 
